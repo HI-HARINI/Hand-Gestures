51b7dad90c5847da11f207c7875b28539e6c38a9
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2021-07-27 19:39:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function speak(){
     speakdata1="The 1st Prediction Is"+prediction1
     speakdata2="The 2nd Prediction Is"+prediction2
 utterthis=new SpeechSynthesisUtterance(speakdata1+speakdata2)
-synth.speak(utterThis)
+synth.speak(utterthis)
 }
 function check(){
     img=document.getElementById("captureimg");
@@ -35,5 +35,28 @@ if(error){
 else{
     console.log(results);
     document.getElementById("result1").innerHTML=results[0].label
+    document.getElementById("result2").innerHTML=results[1].label
+    prediction1=results[0].label
+    prediction2=results[1].label
+speak()
+if (results[0].label=="Best") {
+    document.getElementById("emoji1").innerHTML="&#128077;"
+}
+if (results[0].label=="Amazing") {
+    document.getElementById("emoji1").innerHTML="&#128076;"
+}
+if (results[0].label=="Victory") {
+    document.getElementById("emoji1").innerHTML="&#9996;"
+}
+
+if (results[1].label=="Best") {
+    document.getElementById("emoji2").innerHTML="&#128077;"
+}
+if (results[1].label=="Amazing") {
+    document.getElementById("emoji2").innerHTML="&#128076;"
+}
+if (results[1].label=="Victory") {
+    document.getElementById("emoji2").innerHTML="&#9996;"
+}
 }
 }
